0ff6ee5b918990ca14105111e8fc34f838b684aa
hrmega202301-spec <hrmega202301@gmail.com>
2026-05-07 12:58:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String greeting = "Hello ";
+        String nameUser = "Artem Fatkhutdinov";
+        System.out.println(greeting + nameUser);
+    }
+}
